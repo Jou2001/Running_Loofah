4924602093b1d5f12a0095ce56d6e6c963552777
Jou2001 <emilyang1210@gmail.com>
2023-08-07 15:49:35 +0800

    Update tempAttack

--- tempAttack.py
+++ tempAttack.py
@@ -6,15 +6,21 @@ cam = cv2.VideoCapture(0)
 mp_pose = mp.solutions.pose
 mp_drawing = mp.solutions.drawing_utils
 pose = mp_pose.Pose()
-status = False
-h = 0
-w = 0
 
-def get_mid_point(a, b, mid):
-    mid.x = (a.x + b.x)/2
-    mid.y = (a.y + b.y)/2
-    mid.z = (a.z + b.z)/2
+class Coordinate:
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+def get_mid_point(a, b):
+    mid = Coordinate()
+    mid.x = (a.x + b.x) / 2
+    mid.y = (a.y + b.y) / 2
+    mid.z = (a.z + b.z) / 2
     return mid
+
+
 
 def get_vector(a, b, c):
     ba = np.array([a.x - b.x, a.y - b.y, a.z - b.z])
@@ -23,93 +29,96 @@ def get_vector(a, b, c):
 
 def angle_between_points(a, b, c):
     ba, bc = get_vector(a, b, c)
-    dot = np.dot(ba, bc) 
+    dot = np.dot(ba, bc) # 內積(distance_ba * distance_bc * cos0)
     distance_ba = np.linalg.norm(ba)
     distance_bc = np.linalg.norm(bc)
+
     cosine_angle = dot / (distance_ba * distance_bc)
-    angle_rad = np.arccos(cosine_angle)
-    angel_deg = np.degrees(angle_rad)
+    angle_rad = np.arccos(cosine_angle) # 算出0 (弧度表示)
+    angel_deg = np.degrees(angle_rad) # 將弧度轉為度數
     return angel_deg
 
-def get_distance(a, b):
-    return np.linalg.norm(np.array([a.x - b.x, a.y - b.y, a.z - b.z]))
-
 def main():
-    global h, w, status
-    flag = False
     if not cam.isOpened():
         print("Camera not open")
         exit()
 
-    cv2.namedWindow('frame', 0) 
-    cv2.resizeWindow('frame', 600, 500)
+    cv2.namedWindow('frame', 0) # 設定視窗名稱
+    cv2.resizeWindow('frame', 600, 500)  #重設視窗大小
 
-    while not flag:
+    while True:
         ret, frame = cam.read()
         if not ret:
             print("Read Error")
             break
-        frame = cv2.flip(frame, 1)
+        frame = cv2.flip(frame, 1) #矩陣左右翻轉   ******
         rgbframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = pose.process(rgbframe)
-        h, w, _ = frame.shape
+        results = pose.process(rgbframe) # 從影像增測姿勢    
+        h, w, _ = frame.shape # (480, 640, 3)
         preview = frame.copy()
 
         if results.pose_world_landmarks:
             mp_drawing.draw_landmarks(preview, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-
-            # Landmarks for arms and legs
+            # Hand
             wrist_left = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST]
             wrist_right = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST]
             elbow_left = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW]
             elbow_right = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW]
             shoulder_left = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER]
             shoulder_right = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER]
+
+            # Leg
             hip_left = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.LEFT_HIP]
             knee_left = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.LEFT_KNEE]
             hip_right = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP]
             knee_right = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_KNEE]
-            ankle_left = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ANKLE]
-            ankle_right = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ANKLE]
-            hip_middle = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.LEFT_HIP]
-            hip_middle = get_mid_point(hip_left, hip_right, hip_middle)
+            hip_middle = get_mid_point(hip_left, hip_right)
 
-            # Calculate angles
+
+            print("hip_left", hip_left)
+            print("hip_right", hip_right)
+            print("hip_middle", hip_middle.x, hip_middle.y, hip_middle.z)
+
             angle_left_hand = angle_between_points(wrist_left, elbow_left, shoulder_left)
             angle_right_hand = angle_between_points(wrist_right, elbow_right, shoulder_right)
-            angle_left_knee = angle_between_points(hip_left, knee_left, ankle_left)
-            angle_right_knee = angle_between_points(hip_right, knee_right, ankle_right)
+            angle_left_leg = angle_between_points(hip_left, knee_left, hip_middle)
+            angle_right_leg = angle_between_points(hip_right, knee_right, hip_middle)
             angle_two_leg = angle_between_points(knee_left, hip_middle, knee_right)
+            
+            is_lunge_step = abs(angle_left_leg - angle_right_leg) > 30  # Define your threshold for lunge step here
 
-            # Calculate distances
-            distance_knee_hip_left = get_distance(hip_left, knee_left)
-            distance_knee_hip_right = get_distance(hip_right, knee_right)
-            distance_shoulder_hip_left = get_distance(hip_left, shoulder_left)
-            distance_shoulder_hip_right = get_distance(hip_right, shoulder_right)
+            # Use angle_left_leg and angle_right_leg in your logic instead of angle_left_leg and angle_right_leg
+            # GREEN
+            if (angle_left_leg >= 15 and angle_left_leg <= 20) and (angle_right_leg >= 15 and angle_right_leg <= 20) and  (angle_two_leg >= 50 and angle_two_leg <= 120): 
+                #(angle_left_hand <= 180 and angle_left_hand >= 150) and (angle_right_hand <= 180 and angle_right_hand >= 150) and
+                cv2.putText(preview, "Left Angle: {:f}".format(angle_left_leg), (300, 360), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+                cv2.putText(preview, "Right Angle: {:f}".format(angle_right_leg), (300, 410), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+                cv2.putText(preview, "Right Hand Angle: {:f}".format(angle_right_hand), (300, 460), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+                cv2.putText(preview, "Two Leg Angle: {:f}".format(angle_two_leg), (300, 510), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
 
-            # Good posture
-            if (angle_left_knee > 80 and angle_left_knee < 120) and (angle_right_knee > 80 and angle_right_knee < 120) and \
-                (distance_knee_hip_left < 0.1 and distance_shoulder_hip_left < 0.1) and \
-                (distance_knee_hip_right < 0.1 and distance_shoulder_hip_right < 0.1):
-                status = "Good posture"
+            # YELLOW   
+            elif (( angle_left_leg >= 80 and angle_left_leg <= 90 ) or (angle_left_leg > 120 and angle_left_leg < 130)) and ( ( angle_right_leg >= 80 and angle_right_leg <= 90 ) or (angle_right_leg > 120 and angle_right_leg < 130)) and  (angle_two_leg >= 60 and angle_two_leg < 90) and is_lunge_step: # 黃色
+                cv2.putText(preview, "Left Angle: {:f} ".format(angle_left_leg), (300, 360), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 1, cv2.LINE_AA)
+                cv2.putText(preview, "Right Angle: {:f}".format(angle_right_leg), (300, 410), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 1, cv2.LINE_AA)
+                cv2.putText(preview, "Right Hand Angle: {:f}".format(angle_right_hand), (300, 460), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 1, cv2.LINE_AA)
+                cv2.putText(preview, "Two Leg Angle: {:f}".format(angle_two_leg), (300, 510), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
 
-            # Almost there
-            elif (angle_left_knee > 60 and angle_left_knee < 150) and (angle_right_knee > 60 and angle_right_knee < 150) and \
-                (distance_knee_hip_left < 0.2 and distance_shoulder_hip_left < 0.2) and \
-                (distance_knee_hip_right < 0.2 and distance_shoulder_hip_right < 0.2):
-                status = "Almost there"
-
-            # Not good posture
+            # RED
             else:
-                status = "Not good posture"
-
-        cv2.putText(preview, status, (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                cv2.putText(preview, "Left Angle: {:f}".format(angle_left_leg), (300, 360), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+                cv2.putText(preview, "Right Angle: {:f}".format(angle_right_leg), (300, 410), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+                cv2.putText(preview, "Right Hand Angle: {:f}".format(angle_right_hand), (300, 460), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+                cv2.putText(preview, "Two Leg Angle: {:f}".format(angle_two_leg), (300, 510), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+            
 
         cv2.imshow('frame', preview)
-        key = cv2.waitKey(10) & 0xFF
-        if key == 27:
-            flag = True
+        
+        if cv2.waitKey(1) & 0xFF == ord(' '):
+            break
+
+    # release camera
+    cam.release()
     cv2.destroyAllWindows()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
